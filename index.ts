ee0b78bfbe412f7d87abed8901eda1ee9d84ac61
SATHISH-ASUS\sathi <sathish662001@gmail.com>
2025-03-09 21:28:09 +0530

    fix(add tsup for root): make changes to the root

---
+++ index.ts
@@ -0,0 +1,1 @@
+export { default as Select } from './packages/select/src/select';
---
+++ tsup.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['index.ts'],
+  format: ['cjs', 'esm'],
+  dts: true,
+  clean: true,
+  splitting: false,
+  sourcemap: true,
+  minify: false,
+  external: ['react', 'react-dom'],
+  noExternal: [],
+  loader: {
+    '.css': 'file',
+  },
+});
